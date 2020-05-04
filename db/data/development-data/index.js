aa80fbb9dfe0ba627fb9ed523e23ae3fa4ad05f3
Petru <petru@Petrus-MacBook.local>
2020-05-04 14:31:07 +0100

    created index file for dev data

---
+++ db/data/development-data/index.js
@@ -0,0 +1,8 @@
+const devData = {
+  articles: require("./articles"),
+  comments: require("./comments"),
+  topics: require("./topics"),
+  users: require("./users"),
+};
+
+module.exports = devData;
