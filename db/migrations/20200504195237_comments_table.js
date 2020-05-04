d8a7f2575997566366ebd53eb8e35126bd824fa5
Petru <petru@Petrus-MacBook.local>
2020-05-04 20:07:32 +0100

    finished writing the comments table

--- db/migrations/20200504195237_comments_table.js
+++ db/migrations/20200504195237_comments_table.js
@@ -1,8 +1,24 @@
+exports.up = function (knex) {
+  /*
+  This function will create the 'comments' table. It has the columns 'comment_id' as a primary numerical key, 'author', 'article_id', 'votes', 'created_at', and 'body'.
+  */
+  console.log(
+    "Created the 'comments' table with the columns 'comment_id', 'author', 'article_id', 'votes', 'created_at', and 'body'."
+  );
 
-exports.up = function(knex) {
-  
+  return knex.schema.createTable("comments", (commentsTable) => {
+    commentsTable.increments("comment_id").primary();
+    commentsTable.text("author");
+    commentsTable.text("article_id");
+    commentsTable.integer("votes").defaultTo(0);
+    commentsTable
+      .timestamp("created_at", { precision: 6 })
+      .defaultTo(knex.fn.now(6));
+    commentsTable.text("body");
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  console.log("Deleted the 'comments' table.");
+  return knex.schema.dropTable("comments");
 };
