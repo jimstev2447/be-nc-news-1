9f2b067ea1702f26b495eb05121fd369bc6aeabf
Petru <petru@Petrus-MacBook.local>
2020-05-04 15:04:18 +0100

    writing index file

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -2,14 +2,14 @@ const {
   topicData,
   articleData,
   commentData,
-  userData
-} = require('../data/index.js');
+  userData,
+} = require("../data/data-index.js");
 
-const { formatDates, formatComments, makeRefObj } = require('../utils/utils');
+const { formatDates, formatComments, makeRefObj } = require("../utils/utils");
 
-exports.seed = function(knex) {
-  const topicsInsertions = knex('topics').insert(topicData);
-  const usersInsertions = knex('users').insert(userData);
+exports.seed = function (knex) {
+  const topicsInsertions = knex("topics").insert(topicData);
+  const usersInsertions = knex("users").insert(userData);
 
   return Promise.all([topicsInsertions, usersInsertions])
     .then(() => {
@@ -22,7 +22,7 @@ exports.seed = function(knex) {
       Your comment insertions will depend on information from the seeded articles, so make sure to return the data after it's been seeded.
       */
     })
-    .then(articleRows => {
+    .then((articleRows) => {
       /* 
 
       Your comment data is currently in the incorrect format and will violate your SQL schema. 
@@ -34,6 +34,8 @@ exports.seed = function(knex) {
 
       const articleRef = makeRefObj(articleRows);
       const formattedComments = formatComments(commentData, articleRef);
-      return knex('comments').insert(formattedComments);
+      return knex("comments").insert(formattedComments);
     });
 };
+
+console.log("this is : ", articleData);
