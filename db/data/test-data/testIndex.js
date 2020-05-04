12123abb1066a3ed7cc8440514a657e5018a41f4
Petru <petru@Petrus-MacBook.local>
2020-05-04 14:33:49 +0100

    writing index files

---
+++ db/data/test-data/testIndex.js
@@ -0,0 +1,8 @@
+const testData = {
+  articles: require("./articles"),
+  comments: require("./comments"),
+  topics: require("./topics"),
+  users: require("./users"),
+};
+
+module.exports = testData;
