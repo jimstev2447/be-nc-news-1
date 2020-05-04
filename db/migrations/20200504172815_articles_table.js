dc1ca3acb8dfdb58e5fc97a9fa199e41d79ec3c1
Petru <petru@Petrus-MacBook.local>
2020-05-04 18:03:16 +0100

    working on articles table

--- db/migrations/20200504172815_articles_table.js
+++ db/migrations/20200504172815_articles_table.js
@@ -1,8 +1,19 @@
+exports.up = function (knex) {
+  /*
+  This function will create the 'articles' table. It has the columns 'article_id' as a primary numerical key, 'title', 'body', 'votes', 'topic', 'author' and 'created_at'. 
+  */
+  console.log(
+    "Created the 'articles' table with the columns 'article_id', 'title', 'body', 'votes', 'topic', 'author' and 'created_at'."
+  );
 
-exports.up = function(knex) {
-  
+  return knex.schema.createTable("articles", (articlesTable) => {
+    articlesTable.increments("article_id").primary();
+    articlesTable.text("title");
+    articlesTable.text("body");
+    articlesTable.integer("votes").defaultTo(0);
+    articlesTable.text("topic");
+    articlesTable.foreign("topic").references("topics.slug");
+  });
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {};
