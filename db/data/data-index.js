c3ac1ceccb37a5b0673e1274fc57149e4e2d3164
Petru <petru@Petrus-MacBook.local>
2020-05-04 14:44:59 +0100

    writing index files

---
+++ db/data/data-index.js
@@ -0,0 +1,8 @@
+const ENV = process.env.NODE_ENV || "development";
+
+const data = {
+  development: require("./development-data/dev-data-index"),
+  test: require("./test-data/test-data-index"),
+};
+
+module.exports = data;
