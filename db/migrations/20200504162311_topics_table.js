d64cc7fb333623f9be1365030691e7ccc9121b87
Petru <petru@Petrus-MacBook.local>
2020-05-04 17:11:02 +0100

    working on migrations

--- db/migrations/20200504162311_topics_table.js
+++ db/migrations/20200504162311_topics_table.js
@@ -2,8 +2,7 @@ exports.up = function (knex) {
   /*
   This function will create the 'topics' table
   which is the table in the database that all other
-  tables reference and should therefore be created
-  first. The requirements for this table are that it should have a 'slug' field which should take unique strings, which function as primary keys and it should also have a 'description' field which takes a string that gives a brief description of a given topic. 
+  tables reference. It has the columns 'slug' as a primary key and 'description.' 
   */
   console.log(
     "Created the 'topics' table with the columns 'slug' and 'description'."
