a2fa287995b0c631042d4b85a183c13ddc398ca4
Petru <petru@Petrus-MacBook.local>
2020-05-04 20:36:00 +0100

    started makeRefObj function

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,9 @@
-exports.formatDates = list => {};
+exports.formatDates = (list) => {};
 
-exports.makeRefObj = list => {};
+exports.makeRefObj = (list) => {
+  let refObj = {};
+  list.forEach((article) => (refObj[article.title] = article.article_id));
+  return refObj;
+};
 
 exports.formatComments = (comments, articleRef) => {};
