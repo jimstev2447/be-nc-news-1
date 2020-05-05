ca90b1c009576f7dc1adc8c69bb980232946d84e
Petru <petru@Petrus-MacBook.local>
2020-05-05 18:29:59 +0100

    properly set up the tests for the get all topics method

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,22 +5,27 @@ const app = require("../app.js");
 const knex = require("../db/data/connection.js");
 
 describe.only("Testing GET methods", () => {
-  afterAll(() => {
-    return knex.destroy();
-  });
-  test.only("Sends a response containing all topics to the user", () => {
-    return request(app)
-      .get("/api/topics/")
-      .then((res) => {
-        expect(res.body.topics).toEqual([
-          { slug: "mitch", description: "The man, the Mitch, the legend" },
-          { slug: "cats", description: "Not dogs" },
-          { slug: "paper", description: "what books are made of" },
-        ]);
-      });
+  afterAll(() => knex.destroy());
+  describe.only("Testing GET methods for 'topics'", () => {
+    test.only("Sends a response containing all topics to the user when it is passed the path '/api/topics/'", () => {
+      return request(app)
+        .get("/api/topics/")
+        .then(({ body: { topics } }) => {
+          topics.forEach((topic) => {
+            expect(topic).toHaveProperty("slug");
+            expect(topic).toHaveProperty("description");
+          });
+          expect(topics).toEqual([
+            { slug: "mitch", description: "The man, the Mitch, the legend" },
+            { slug: "cats", description: "Not dogs" },
+            { slug: "paper", description: "what books are made of" },
+          ]);
+          expect(typeof topics).toBe("object");
+        });
+    });
   });
 });
 
-xdescribe("Testing POST methods", () => {});
-xdescribe("Testing PATCH methods", () => {});
-xdescribe("Testing DELETE methods", () => {});
+// xdescribe("Testing POST methods", () => {});
+// xdescribe("Testing PATCH methods", () => {});
+// xdescribe("Testing DELETE methods", () => {});
