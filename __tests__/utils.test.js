8d4216f37d2948dda450c6589e777b7a417d748c
Petru <petru@Petrus-MacBook.local>
2020-05-05 09:10:02 +0100

    written and tested makeRefObj

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -2,10 +2,28 @@ const {
   formatDates,
   makeRefObj,
   formatComments,
-} = require('../db/utils/utils');
+} = require("../db/utils/utils");
 
-describe('formatDates', () => {});
+describe("formatDates", () => {});
 
-describe('makeRefObj', () => {});
+describe("makeRefObj", () => {
+  test("returns an empty object, when passed an empty array", () => {
+    const list = [];
+    const actual = makeRefObj(list);
+    const expected = {};
+    expect(actual).toEqual(expected);
+  });
+  test("it does not mutate the original list", () => {
+    const list = [];
+    makeRefObj(list);
+    expect(list).toEqual([]);
+  });
+  test("it creates a reference object from an array of objects using different arguments", () => {
+    const list = [{ article_id: 1, title: "A" }];
+    expect(makeRefObj(list)).toEqual({
+      A: 1,
+    });
+  });
+});
 
-describe('formatComments', () => {});
+describe("formatComments", () => {});
