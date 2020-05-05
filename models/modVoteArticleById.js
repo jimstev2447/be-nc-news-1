f7fdbb7df00dd70b3015db90b9cc05876db9b3ae
Petru <petru@Petrus-MacBook.local>
2020-05-05 23:28:52 +0100

    commit

--- models/modVoteArticleById.js
+++ models/modVoteArticleById.js
@@ -1,3 +1,15 @@
-const modVoteArticleById = () => {};
+const knex = require("../db/data/connection.js");
+
+const modVoteArticleById = () => {
+  return knex("articles")
+    .select("articles.*")
+    .count({ comment_count: "comment_id" })
+    .leftJoin("comments", "comments.article_id", "articles.article_id")
+    .groupBy("articles.article_id")
+    .where("articles.article_id", "=", `${article_id}`)
+    .then(([article]) => {
+      return article;
+    });
+};
 
 module.exports = modVoteArticleById;
