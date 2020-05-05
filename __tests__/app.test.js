f99ebb6d69852e88d121d934ec51d71fabb7cb81
Petru <petru@Petrus-MacBook.local>
2020-05-05 16:38:20 +0100

    writing routes for server

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,11 @@
 process.env.NODE_ENV = "test";
 
 const request = require("supertest");
-const app = require("../app");
+const app = require("../app.js");
 
-describe("Testing GET methods", () => {});
+describe("Testing GET methods", () => {
+  test("works", () => {});
+});
+describe("Testing POST methods", () => {});
+describe("Testing PATCH methods", () => {});
+describe("Testing DELETE methods", () => {});
