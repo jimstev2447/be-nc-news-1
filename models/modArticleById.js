7f8dc4834b720cd2cdc70c589b9f3a3c766271cb
Petru <petru@Petrus-MacBook.local>
2020-05-05 22:22:16 +0100

    commit

--- models/modArticleById.js
+++ models/modArticleById.js
@@ -1,10 +1,9 @@
 const knex = require("../db/data/connection.js");
 
 const modArticlerById = (article_id) => {
-  return knex
+  return knex("articles")
     .select("articles.*")
     .count({ comment_count: "comment_id" })
-    .from("articles")
     .leftJoin("comments", "comments.article_id", "articles.article_id")
     .groupBy("articles.article_id")
     .where("articles.article_id", "=", `${article_id}`)
