9e50943856cfef5292954a75b5752de855dfb202
Petru <petru@Petrus-MacBook.local>
2020-05-05 18:15:52 +0100

    setup tests to run without a failing to exit

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,12 +2,25 @@ process.env.NODE_ENV = "test";
 
 const request = require("supertest");
 const app = require("../app.js");
+const knex = require("../db/data/connection.js");
 
-describe("Testing GET methods", () => {
-  test("works", () => {
-    return request(app).get("/api/topics");
+describe.only("Testing GET methods", () => {
+  afterAll(() => {
+    return knex.destroy();
+  });
+  test.only("Sends a response containing all topics to the user", () => {
+    return request(app)
+      .get("/api/topics/")
+      .then((res) => {
+        expect(res.body.topics).toEqual([
+          { slug: "mitch", description: "The man, the Mitch, the legend" },
+          { slug: "cats", description: "Not dogs" },
+          { slug: "paper", description: "what books are made of" },
+        ]);
+      });
   });
 });
-describe("Testing POST methods", () => {});
-describe("Testing PATCH methods", () => {});
-describe("Testing DELETE methods", () => {});
+
+xdescribe("Testing POST methods", () => {});
+xdescribe("Testing PATCH methods", () => {});
+xdescribe("Testing DELETE methods", () => {});
--- controllers/getAllTopics.js
+++ controllers/getAllTopics.js
@@ -2,7 +2,7 @@ const modAllTopics = require("../models/modAllTopics");
 
 const getAllTopics = (req, res) => {
   modAllTopics().then((allTopics) => {
-    res.status(200).send({ allTopics });
+    res.status(200).send({ topics: allTopics });
   });
 };
 
