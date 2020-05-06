4aeac66f55c738222e427d450481779a6ddcfe30
Petru <petru@Petrus-MacBook.local>
2020-05-06 12:21:47 +0100

    added error handling for delete method

--- models/modDeleteCommentsById.js
+++ models/modDeleteCommentsById.js
@@ -1,7 +1,14 @@
 const knex = require("../db/data/connection.js");
 
 const modDeleteCommentsById = (comment_id) => {
-  return knex("comments").where({ comment_id }).del();
+  return knex("comments")
+    .where({ comment_id })
+    .del()
+    .then((delCount) => {
+      if (delCount === 1) {
+        return Promise.reject({ status: 404, message: "Comment not found." });
+      }
+    });
 };
 
 module.exports = modDeleteCommentsById;
