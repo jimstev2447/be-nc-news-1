840b4d65671eb849d90604a9444008263eaccf8a
Petru <petru@Petrus-MacBook.local>
2020-05-06 01:27:21 +0100

    commit

--- routes/articlesRouter/articlesRouter.js
+++ routes/articlesRouter/articlesRouter.js
@@ -2,7 +2,9 @@ const articlesRouter = require("express").Router();
 const getArticleById = require("../../controllers/getArticleById");
 // const patchArticleVoteById = require("../../controllers/patchArticleVoteById");
 const getCommentsByArticleId = require("../../controllers/getCommentsByArticleId");
+const getAllArticles = require("../../controllers/getAllArticles.js");
 
+articlesRouter.route("/").get(getAllArticles);
 articlesRouter.route("/:article_id").get(getArticleById);
 articlesRouter.route("/:article_id/comments").get(getCommentsByArticleId);
 // .patch(patchArticleVoteById);
