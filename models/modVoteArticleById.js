e7e7a3c006ca06ea4cc386f5049e2a6a2b5cff68
Petru <petru@Petrus-MacBook.local>
2020-05-06 13:56:50 +0100

    commit

--- models/modVoteArticleById.js
+++ models/modVoteArticleById.js
@@ -1,7 +1,14 @@
 const knex = require("../db/data/connection.js");
 
 const modVoteArticleById = (article_id, incVotes) => {
-  console.log("hello");
+  return knex("articles")
+    .where("articles.article_id", "=", article_id)
+    .increment("votes", incVotes)
+    .returning("*")
+    .then((article) => {
+      console.log(article);
+      return article;
+    });
 };
 
 module.exports = modVoteArticleById;
