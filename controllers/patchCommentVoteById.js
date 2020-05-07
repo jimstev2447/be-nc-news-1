2e5c7f6ba9d2657a47cd0cd9adfb05e78c7134a2
Petru <petru@Petrus-MacBook.local>
2020-05-07 16:17:33 +0100

    working through errors

--- controllers/patchCommentVoteById.js
+++ controllers/patchCommentVoteById.js
@@ -3,7 +3,10 @@ const modPatchCommentVoteById = require("../models/modPatchCommentVoteById");
 const patchCommentVoteById = (req, res, next) => {
   const comment_id = req.params.comment_id;
   const incVotes = req.body.inc_votes;
-  if (typeof parseInt(incVotes) !== "number") {
+  if (
+    typeof parseInt(incVotes) !== "number" ||
+    typeof parseInt(comment_id) !== "number"
+  ) {
     next({ status: 400, message: "bad request" });
   } else {
     modPatchCommentVoteById(comment_id, incVotes)
