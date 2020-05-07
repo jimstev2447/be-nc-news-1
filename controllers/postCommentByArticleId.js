cb43c218cc2c3298b07e94c7d369993892780263
Petru <petru@Petrus-MacBook.local>
2020-05-07 16:05:45 +0100

    wokring through errors

--- controllers/postCommentByArticleId.js
+++ controllers/postCommentByArticleId.js
@@ -3,12 +3,15 @@ const modPostCommentByArticleId = require("../models/modPostCommentByArticleId")
 const postCommentByArticleId = (req, res, next) => {
   const { article_id } = req.params;
   const { body, username } = req.body;
-
-  modPostCommentByArticleId(article_id, body, username)
-    .then((updatedComment) => {
-      res.status(201).send({ comment: updatedComment });
-    })
-    .catch(next);
+  if (!article_id || !body || !username) {
+    next({ status: 400, message: "bad request" });
+  } else {
+    modPostCommentByArticleId(article_id, body, username)
+      .then((updatedComment) => {
+        res.status(201).send({ comment: updatedComment });
+      })
+      .catch(next);
+  }
 };
 
 module.exports = postCommentByArticleId;
