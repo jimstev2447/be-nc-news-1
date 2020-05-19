54d240535da1958a335518313562002489938052
Petru <petru@Petrus-MacBook.local>
2020-05-19 09:09:17 +0100

    added heroku hosting

--- db/data/data-index.js
+++ db/data/data-index.js
@@ -1,6 +1,7 @@
 const ENV = process.env.NODE_ENV || "development";
 
 const data = {
+  production: require("./development-data/dev-data-index"),
   development: require("./development-data/dev-data-index"),
   test: require("./test-data/test-data-index"),
 };
