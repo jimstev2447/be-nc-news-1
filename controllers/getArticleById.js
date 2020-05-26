c6dff06424869c1f2788e5465654088324f082a9
Petru <petru@Petrus-MacBook.local>
2020-05-26 23:19:50 +0100

    changed remote

--- controllers/getArticleById.js
+++ controllers/getArticleById.js
@@ -5,19 +5,14 @@ const getArticleById = (req, res, next) => {
   const { params } = req;
   const article_id = params["article_id"];
 
-  // if (typeof parseInt(article_id) !== "number") {
-  //   next({ status: 400, message: "bad request" });
-  // } else {
   modArticleById(article_id)
     .then((article) => {
       res.status(200).send({ article });
     })
-    // .catch(next); <----this won't work
+
     .catch((error) => {
       next(error);
-      // errorCustomHandler(error, req, res, next); <---- line 17 and 18 will work as well
     });
 };
-//};
 
 module.exports = getArticleById;
